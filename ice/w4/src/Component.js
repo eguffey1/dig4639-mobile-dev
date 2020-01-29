7c4f8114a5c42246ccbd6b7747b82f2f32eaf556
Erickson Guffey <eguffeyarmy2@gmail.com>
2020-01-29 12:19:15 -0500

    Co-authored-by: leidyp96 <leidypulido96@gmail.com> Co-authored-by: NathanS97 <n.snodgrass97@knights.ucf.edu> Co-authored-by: Gab-Sinclair <gabrielle23@knights.ucf.edu>

--- ice/w4/src/Component.js
+++ ice/w4/src/Component.js
@@ -1,14 +1,17 @@
 class Component {
-    constructor(_button) {
-        this.button = _button;
-        console.log("Test");
-        this.button.addEventListener("click", this.onClick)
+    constructor(props) {
+        this.props = props;
+
     }
     onClick() {
         console.log("Clicked");
     }
     render() {
+        this.addOne();
         return `<div></div>`;
+    }
+    addOne() {
+        console.log(this.props.a + 1);
     }
 
 }
