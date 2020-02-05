b0cab2ea5e815efa6104335dc3bc04d941d4286e
Erickson Guffey <eguffeyarmy2@gmail.com>
2020-02-05 14:22:25 -0500

    Completed lab 2

---
+++ labs/2/src/index.js
@@ -0,0 +1,14 @@
+
+import Card from "../components/Card/index.js";
+let element;
+function runOnLoad()
+{
+    // Create a container for us
+    element = document.createElement("div");
+    element.id = "container";
+    document.body.appendChild(element);
+    var newTask = new Card({content:"Sample value provided"});
+    element.appendChild(newTask.render());
+}
+
+window.addEventListener("DOMContentLoaded", runOnLoad);
